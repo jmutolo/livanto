f2f1ec60e184aa806307c9e59e4a51da9b17394a
jmutolo <102764323+jmutolo@users.noreply.github.com>
2023-06-20 17:16:03 -0300

    Add files via upload

---
+++ equipoCarousel.js
@@ -0,0 +1,231 @@
+document.getElementById('equipoSplide').innerHTML = `
+
+<section id="equipo" class="splide" aria-label="Splide Basic HTML Example">
+    <div class="splide__track">
+        <ul class="splide__list">
+            <li class="splide__slide">
+                <div data-w-id="bdd2028f-9f34-ca75-c86a-f3b870344973" class="integranteequipo">
+                    <div class="pfp">
+                        <img src="https://uploads-ssl.webflow.com/6487502c33708c10f0885323/6491f57d789eef3bea91f37d_i.webp" loading="lazy" alt="" class="image-55" />
+                        <img src="https://uploads-ssl.webflow.com/6487502c33708c10f0885323/6491f7c8a7d63abf0fed612e_Logo%20icon.svg" loading="lazy" style="opacity: 0;" alt="" class="bgcircle" />
+                        <div class="borderline"></div>
+                    </div>
+                    <div class="div-block-39">
+                        <div class="text-block-24">Presidente</div>
+                        <div class="text-block-25">Carlos Ortelli</div>
+                    </div>
+                </div>
+            </li>
+            <li class="splide__slide">
+                <div data-w-id="d079fac8-b4c9-89f2-c39a-c3f56ebda99e" class="integranteequipo">
+                    <div class="pfp">
+                        <img src="https://uploads-ssl.webflow.com/6487502c33708c10f0885323/6491fdaa28051eee16e50847_i%20(1).webp" loading="lazy" alt="" class="image-55" />
+                        <img src="https://uploads-ssl.webflow.com/6487502c33708c10f0885323/6491f7c8a7d63abf0fed612e_Logo%20icon.svg" loading="lazy" style="opacity: 0;" alt="" class="bgcircle" />
+                        <div class="borderline"></div>
+                    </div>
+                    <div class="div-block-39">
+                        <div class="text-block-24">Vice presidente</div>
+                        <div class="text-block-25">Jorge Forchino</div>
+                    </div>
+                </div>
+            </li>
+            <li class="splide__slide">
+                <div data-w-id="ef1a6ef0-3a58-546f-56a3-32807e0a4a9f" class="integranteequipo">
+                    <div class="pfp">
+                        <img src="https://uploads-ssl.webflow.com/6487502c33708c10f0885323/6491fed10518abbd338ca134_i%20(2).webp" loading="lazy" alt="" class="image-55" />
+                        <img src="https://uploads-ssl.webflow.com/6487502c33708c10f0885323/6491f7c8a7d63abf0fed612e_Logo%20icon.svg" loading="lazy" style="opacity: 0;" alt="" class="bgcircle" />
+                        <div class="borderline"></div>
+                    </div>
+                    <div class="div-block-39">
+                        <div class="text-block-24">Presidente</div>
+                        <div class="text-block-25">Carlos Ortelli</div>
+                    </div>
+                </div>
+            </li>
+            <li class="splide__slide">
+                <div data-w-id="633bf7f8-58b3-1952-d67f-8b302d6a50c4" class="integranteequipo">
+                    <div class="pfp">
+                        <img src="https://uploads-ssl.webflow.com/6487502c33708c10f0885323/649200534bab743c78da9ba3_i%20(3).webp" loading="lazy" alt="" class="image-55" />
+                        <img src="https://uploads-ssl.webflow.com/6487502c33708c10f0885323/6491f7c8a7d63abf0fed612e_Logo%20icon.svg" loading="lazy" style="opacity: 0;" alt="" class="bgcircle" />
+                        <div class="borderline"></div>
+                    </div>
+                    <div class="div-block-39">
+                        <div class="text-block-24">Presidente</div>
+                        <div class="text-block-25">Carlos Ortelli</div>
+                    </div>
+                </div>
+            </li>
+            <li class="splide__slide">
+                <div data-w-id="66892bc2-16b7-9aef-763f-f8959911d51a" class="integranteequipo">
+                    <div class="pfp">
+                        <img src="https://uploads-ssl.webflow.com/6487502c33708c10f0885323/649200d12ce45656798b7407_i%20(4).webp" loading="lazy" alt="" class="image-55" />
+                        <img src="https://uploads-ssl.webflow.com/6487502c33708c10f0885323/6491f7c8a7d63abf0fed612e_Logo%20icon.svg" loading="lazy" style="opacity: 0;" alt="" class="bgcircle" />
+                        <div class="borderline"></div>
+                    </div>
+                    <div class="div-block-39">
+                        <div class="text-block-24">Presidente</div>
+                        <div class="text-block-25">Carlos Ortelli</div>
+                    </div>
+                </div>
+            </li>
+            <li class="splide__slide">
+                <div data-w-id="06c05ddb-9671-cba0-3092-3294a97ac1d5" class="integranteequipo">
+                    <div class="pfp">
+                        <img src="https://uploads-ssl.webflow.com/6487502c33708c10f0885323/649200e323eb33ca4d226bf7_i%20(5).webp" loading="lazy" alt="" class="image-55" />
+                        <img src="https://uploads-ssl.webflow.com/6487502c33708c10f0885323/6491f7c8a7d63abf0fed612e_Logo%20icon.svg" loading="lazy" style="opacity: 0;" alt="" class="bgcircle" />
+                        <div class="borderline"></div>
+                    </div>
+                    <div class="div-block-39">
+                        <div class="text-block-24">Presidente</div>
+                        <div class="text-block-25">Carlos Ortelli</div>
+                    </div>
+                </div>
+            </li>
+            <li class="splide__slide">
+                <div data-w-id="dbd89add-14b9-13c9-677c-5e98844dd5fc" class="integranteequipo">
+                    <div class="pfp">
+                        <img src="https://uploads-ssl.webflow.com/6487502c33708c10f0885323/649200f08f6742dd55b08faf_i%20(6).webp" loading="lazy" alt="" class="image-55" />
+                        <img src="https://uploads-ssl.webflow.com/6487502c33708c10f0885323/6491f7c8a7d63abf0fed612e_Logo%20icon.svg" loading="lazy" style="opacity: 0;" alt="" class="bgcircle" />
+                        <div class="borderline"></div>
+                    </div>
+                    <div class="div-block-39">
+                        <div class="text-block-24">Presidente</div>
+                        <div class="text-block-25">Carlos Ortelli</div>
+                    </div>
+                </div>
+            </li>
+            <li class="splide__slide">
+                <div data-w-id="db76cda7-9cd9-5ff0-c2c8-277cfc71e0be" class="integranteequipo">
+                    <div class="pfp">
+                        <img src="https://uploads-ssl.webflow.com/6487502c33708c10f0885323/64920104fe223da5ad93f337_i%20(7).webp" loading="lazy" alt="" class="image-55" />
+                        <img src="https://uploads-ssl.webflow.com/6487502c33708c10f0885323/6491f7c8a7d63abf0fed612e_Logo%20icon.svg" loading="lazy" style="opacity: 0;" alt="" class="bgcircle" />
+                        <div class="borderline"></div>
+                    </div>
+                    <div class="div-block-39">
+                        <div class="text-block-24">Presidente</div>
+                        <div class="text-block-25">Carlos Ortelli</div>
+                    </div>
+                </div>
+            </li>
+            <li class="splide__slide">
+                <div data-w-id="2fae0462-7a21-25da-7b17-06ee584f9ef7" class="integranteequipo">
+                    <div class="pfp">
+                        <img src="https://uploads-ssl.webflow.com/6487502c33708c10f0885323/64920111bae0f355554af75b_i%20(8).webp" loading="lazy" alt="" class="image-55" />
+                        <img src="https://uploads-ssl.webflow.com/6487502c33708c10f0885323/6491f7c8a7d63abf0fed612e_Logo%20icon.svg" loading="lazy" style="opacity: 0;" alt="" class="bgcircle" />
+                        <div class="borderline"></div>
+                    </div>
+                    <div class="div-block-39">
+                        <div class="text-block-24">Presidente</div>
+                        <div class="text-block-25">Carlos Ortelli</div>
+                    </div>
+                </div>
+            </li>
+            <li class="splide__slide">
+                <div data-w-id="554c49c6-57c4-6993-2dc4-0df9f1025184" class="integranteequipo">
+                    <div class="pfp">
+                        <img src="https://uploads-ssl.webflow.com/6487502c33708c10f0885323/6492011de1b0588da9b8d808_i%20(9).webp" loading="lazy" alt="" class="image-55" />
+                        <img src="https://uploads-ssl.webflow.com/6487502c33708c10f0885323/6491f7c8a7d63abf0fed612e_Logo%20icon.svg" loading="lazy" style="opacity: 0;" alt="" class="bgcircle" />
+                        <div class="borderline"></div>
+                    </div>
+                    <div class="div-block-39">
+                        <div class="text-block-24">Presidente</div>
+                        <div class="text-block-25">Carlos Ortelli</div>
+                    </div>
+                </div>
+            </li>
+            <li class="splide__slide">
+                <div data-w-id="97011b54-5e3d-e656-c9aa-37c01292e832" class="integranteequipo">
+                    <div class="pfp">
+                        <img src="https://uploads-ssl.webflow.com/6487502c33708c10f0885323/64920129fc4474ac8d6f438b_i%20(10).webp" loading="lazy" alt="" class="image-55" />
+                        <img src="https://uploads-ssl.webflow.com/6487502c33708c10f0885323/6491f7c8a7d63abf0fed612e_Logo%20icon.svg" loading="lazy" style="opacity: 0;" alt="" class="bgcircle" />
+                        <div class="borderline"></div>
+                    </div>
+                    <div class="div-block-39">
+                        <div class="text-block-24">Presidente</div>
+                        <div class="text-block-25">Carlos Ortelli</div>
+                    </div>
+                </div>
+            </li>
+            <li class="splide__slide">
+                <div data-w-id="af3a8ea6-9189-2318-61a7-9c729f93ec07" class="integranteequipo">
+                    <div class="pfp">
+                        <img src="https://uploads-ssl.webflow.com/6487502c33708c10f0885323/649201344d066985d0237266_i%20(11).webp" loading="lazy" alt="" class="image-55" />
+                        <img src="https://uploads-ssl.webflow.com/6487502c33708c10f0885323/6491f7c8a7d63abf0fed612e_Logo%20icon.svg" loading="lazy" style="opacity: 0;" alt="" class="bgcircle" />
+                        <div class="borderline"></div>
+                    </div>
+                    <div class="div-block-39">
+                        <div class="text-block-24">Presidente</div>
+                        <div class="text-block-25">Carlos Ortelli</div>
+                    </div>
+                </div>
+            </li>
+            <li class="splide__slide">
+                <div data-w-id="39cb8584-5245-7cc6-c866-7ac7ae82d98a" class="integranteequipo">
+                    <div class="pfp">
+                        <img src="https://uploads-ssl.webflow.com/6487502c33708c10f0885323/6492009ca8139f378675933f_i%20(12).webp" loading="lazy" alt="" class="image-55" />
+                        <img src="https://uploads-ssl.webflow.com/6487502c33708c10f0885323/6491f7c8a7d63abf0fed612e_Logo%20icon.svg" loading="lazy" style="opacity: 0;" alt="" class="bgcircle" />
+                        <div class="borderline"></div>
+                    </div>
+                    <div class="div-block-39">
+                        <div class="text-block-24">Presidente</div>
+                        <div class="text-block-25">Carlos Ortelli</div>
+                    </div>
+                </div>
+            </li>
+            <li class="splide__slide">
+                <div data-w-id="71ccef16-93a5-e401-9501-8353850c1e58" class="integranteequipo">
+                    <div class="pfp">
+                        <img src="https://uploads-ssl.webflow.com/6487502c33708c10f0885323/64920157dcfe55605ee6cbb7_i%20(13).webp" loading="lazy" alt="" class="image-55" />
+                        <img src="https://uploads-ssl.webflow.com/6487502c33708c10f0885323/6491f7c8a7d63abf0fed612e_Logo%20icon.svg" loading="lazy" style="opacity: 0;" alt="" class="bgcircle" />
+                        <div class="borderline"></div>
+                    </div>
+                    <div class="div-block-39">
+                        <div class="text-block-24">Presidente</div>
+                        <div class="text-block-25">Carlos Ortelli</div>
+                    </div>
+                </div>
+            </li>
+            <li class="splide__slide">
+                <div data-w-id="5e671c12-5944-88f5-d94d-73703ea599b7" class="integranteequipo">
+                    <div class="pfp">
+                        <img src="https://uploads-ssl.webflow.com/6487502c33708c10f0885323/64920162a8139f3786767fc7_i%20(14).webp" loading="lazy" alt="" class="image-55" />
+                        <img src="https://uploads-ssl.webflow.com/6487502c33708c10f0885323/6491f7c8a7d63abf0fed612e_Logo%20icon.svg" loading="lazy" style="opacity: 0;" alt="" class="bgcircle" />
+                        <div class="borderline"></div>
+                    </div>
+                    <div class="div-block-39">
+                        <div class="text-block-24">Presidente</div>
+                        <div class="text-block-25">Carlos Ortelli</div>
+                    </div>
+                </div>
+            </li>
+            <li class="splide__slide">
+                <div data-w-id="81179ac6-ddc9-8a44-de63-ce636929fa40" class="integranteequipo">
+                    <div class="pfp">
+                        <img src="https://uploads-ssl.webflow.com/6487502c33708c10f0885323/6492017827ad8eea2a2c0d7f_i%20(15).webp" loading="lazy" alt="" class="image-55" />
+                        <img src="https://uploads-ssl.webflow.com/6487502c33708c10f0885323/6491f7c8a7d63abf0fed612e_Logo%20icon.svg" loading="lazy" style="opacity: 0;" alt="" class="bgcircle" />
+                        <div class="borderline"></div>
+                    </div>
+                    <div class="div-block-39">
+                        <div class="text-block-24">Presidente</div>
+                        <div class="text-block-25">Carlos Ortelli</div>
+                    </div>
+                </div>
+            </li>
+            <li class="splide__slide">
+                <div data-w-id="09c337f4-6524-bef5-788f-40ab16f3cbef" class="integranteequipo">
+                    <div class="pfp">
+                        <img src="https://uploads-ssl.webflow.com/6487502c33708c10f0885323/64920183015b6feeb903d0bc_i%20(16).webp" loading="lazy" alt="" class="image-55" />
+                        <img src="https://uploads-ssl.webflow.com/6487502c33708c10f0885323/6491f7c8a7d63abf0fed612e_Logo%20icon.svg" loading="lazy" style="opacity: 0;" alt="" class="bgcircle" />
+                        <div class="borderline"></div>
+                    </div>
+                    <div class="div-block-39">
+                        <div class="text-block-24">Presidente</div>
+                        <div class="text-block-25">Carlos Ortelli</div>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>
+</section>
+
+`;
